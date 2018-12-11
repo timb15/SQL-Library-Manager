fe60b5c7465cc10adf44eb7017e0a4a9fe889260
timb15 <43254717+timb15@users.noreply.github.com>
2018-12-10 16:07:19 -0800

    Set up sequelize

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var mainRoute = require('./routes/index');
-var bookRouter = require('./routes/book');
+var bookDetailsRouter = require('./routes/bookDetails');
 var newBookRouter = require('./routes/newBook');
 
 var app = express();
@@ -20,7 +20,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(mainRoute);
-app.use('/book', bookRouter);
+app.use('/book_details', bookDetailsRouter);
 app.use('/new_book', newBookRouter);
 
 // catch 404 and forward to error handler
--- routes/bookDetails.js
+++ routes/bookDetails.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET book details. */
 router.get('/', function(req, res, next) {
-  res.render('book.pug');
+  res.render('book_details');
 });
 
 module.exports = router;
