45d907418d6ab714c558d6fd05d09bcd673bff6a
timb15 <43254717+timb15@users.noreply.github.com>
2018-12-16 18:03:29 -0800

    add search functionality

--- models/book.js
+++ models/book.js
@@ -1,10 +1,24 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
   const Book = sequelize.define('Book', {
-    title: DataTypes.STRING,
-    author: DataTypes.STRING,
+    title: { 
+      type: DataTypes.STRING,
+      validate: {
+        notEmpty: {
+          msg: "Title is required!"
+        }     
+      }
+    },
+    author: { 
+      type:DataTypes.STRING,
+      validate: {
+        notEmpty: {
+          msg: "Author is required!"
+        }       
+      }
+    },
     genre: DataTypes.STRING,
-    year: DataTypes.INTEGER
+    year: DataTypes.INTEGER,
   }, {});
   Book.associate = function(models) {
     // associations can be defined here
--- routes/books.js
+++ routes/books.js
@@ -1,49 +1,135 @@
 var express = require('express');
 var router = express.Router();
 var Book = require('../models').Book;
+var Sequelize = require('sequelize');
+var Op = Sequelize.Op;
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  Book.findAll({order:[['title', 'ASC']]}).then(function(books) {
-    res.render('index', {books});
+  Book.findAll({order: [['title', 'ASC']]}).then(function(books) {
+    if(books) {
+      res.render('index', {books}); 
+    }else {
+      res.send(404, "Page Not Found!");
+    }
+  }).catch(function(err) {
+      res.send(500);
   });
 });
+
+//Display list of books based on search query
+router.get('/search', function (req, res, next) {
+  var query = req.query.query;
+  var searched = true; //used to add home link to search results
+
+  Book.findAll({
+    where: {
+      [Op.or]: [
+        {
+        title: { 
+          [Op.like]: `%${query}%`}
+        },
+        {
+        author:{ 
+          [Op.like]: `%${query}%`}
+        },
+        {
+        genre: { 
+          [Op.like]:`%${query}%`}
+        },
+        {
+        year: { 
+          [Op.like]:`%${query}%`}
+        }
+      ]
+    },  
+    order: [['title', 'ASC']],
+  }).then(function(books) {
+    if(books) {
+      res.render('index', {books, searched});
+    }else {
+      res.send(404, "Page Not Found!");
+    }
+  }).catch(function(err) {
+      res.send(500);
+  });
+});
+
 /*Post new book */
 router.post('/', function(req, res, next) {
   Book.create(req.body).then(function(book) {
-     res.redirect('/books/' + book.id);
-  }); 
+    if(book) {
+      res.redirect('/books/' + book.id);
+    }else {
+      res.send(404);
+    }
+  }).catch(function(err) {
+      if (err.name === "SequelizeValidationError") {
+        res.render('new-book', {book: Book.build(req.body), errors: err.errors});;
+      }else {
+        throw err;
+      }
+  }).catch(function(err) {
+      res.send(500);
+  });
 });
 
 /* Create new book form. */
 router.get('/new', function(req, res, next) {
-  res.render('new_book', {book: Book.build()});
+  res.render('new-book', {book: Book.build()});
 });
 
 
 /*Update book details*/
 router.post('/:id', function(req, res, next) {
   Book.findById(req.params.id).then(function(book) {
-    return book.update(req.body);
-  }).then(function(updatedBook) {
-    res.redirect('/books/' + updatedBook.id);
-  });
+    if(book) {
+      return book.update(req.body); 
+    }else {
+      res.send(404);
+    }
+  }).then(function(book) {
+      res.redirect(`/books/${book.id}`);
+  }).catch(function(err) {
+    if (err.name === "SequelizeValidationError") {
+      var book = req.body;
+      book.id = req.params.id;
+
+      res.render('book-details', {book, errors: err.errors});;
+    }else {
+      throw err;
+    }
+}).catch(function(err) {
+      res.send(500);
+  })
 });
 
 /*Delete book*/
 router.post('/:id/delete', function(req, res, next) {
   Book.findById(req.params.id).then(function(book) {
-    return book.destroy();
+    if(book) {
+      return book.destroy();
+    }else {
+      res.send(404);
+    }
   }).then(function() {
-    res.redirect('/books');
-  });
+      res.redirect('/books');
+  }).catch(function(err) {
+      res.send(500);
+  })
 });
 
 /* GET individual book details. */
 router.get('/:id', function(req, res, next) {
-  Book.findByPk(req.params.id).then(function(book) {
-    res.render('book_details', {book});
-  });  
+  Book.findById(req.params.id).then(function(book) {
+    if (book) {
+      res.render('book-details', {book}); 
+    }else {
+      res.send(404, "Sorry the book you are looking for was not found!");
+    }
+  }).catch(function(err){
+      res.send(500);
+  }); 
 });
 
 module.exports = router;
