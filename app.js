422a2d9ba538e6eab9eb020dc32660bc60e32a97
timb15 <43254717+timb15@users.noreply.github.com>
2019-07-11 12:15:28 -0700

    update sequelize findById to findByPk

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use( '/', routes);
+app.use('/', routes);
 app.use('/books', books);
 
 // catch 404 and forward to error handler
@@ -29,10 +29,9 @@ app.use((err, req, res, next) => {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
-
   // render the error page
   res.status(err.status || 500);
   res.render('page-not-found');
 });
 
- module.exports = app;
+module.exports = app;
--- routes/books.js
+++ routes/books.js
@@ -6,36 +6,38 @@ const Op = Sequelize.Op;
 
 /* GET home page. */
 router.get('/', (req, res, next) => {
-  Book.findAndCountAll({order: [['title', 'ASC']], limit: 7}).then((books) => {
-    if(books) {
+  Book.findAndCountAll({ order: [['title', 'ASC']], limit: 7 }).then((books) => {
+    if (books) {
       res.render('index', {
-        books: books.rows, 
-        count: books.count, 
-        route: 'page', 
-        title: 'All Books'}); 
-    }else {
+        books: books.rows,
+        count: books.count,
+        route: 'page',
+        title: 'All Books'
+      });
+    } else {
       res.send(404, "Page Not Found!");
     }
   }).catch((err) => {
-      res.send(500);
+    res.send(500);
   });
 });
 
 //Display results from clicking pagination link
 router.get('/page', (req, res, next) => {
   const offset = req.query.offset;
-  Book.findAndCountAll({order: [['title', 'ASC']], offset: offset, limit: 7}).then(function(books) {
-    if(books) {
+  Book.findAndCountAll({ order: [['title', 'ASC']], offset: offset, limit: 7 }).then((books) => {
+    if (books) {
       res.render('index', {
-        books: books.rows, 
-        count: books.count, 
-        route: 'page', 
-        title: 'All Books'}); 
-    }else {
+        books: books.rows,
+        count: books.count,
+        route: 'page',
+        title: 'All Books'
+      });
+    } else {
       res.send(404, "Page Not Found!");
     }
   }).catch((err) => {
-      res.send(500);
+    res.send(500);
   });
 });
 
@@ -48,39 +50,44 @@ router.get('/search', (req, res, next) => {
     where: {
       [Op.or]: [
         {
-        title: { 
-          [Op.like]: `%${query}%`}
+          title: {
+            [Op.like]: `%${query}%`
+          }
         },
         {
-        author:{ 
-          [Op.like]: `%${query}%`}
+          author: {
+            [Op.like]: `%${query}%`
+          }
         },
         {
-        genre: { 
-          [Op.like]:`%${query}%`}
+          genre: {
+            [Op.like]: `%${query}%`
+          }
         },
         {
-        year: { 
-          [Op.like]:`%${query}%`}
+          year: {
+            [Op.like]: `%${query}%`
+          }
         }
       ]
-    },  
+    },
     order: [['title', 'ASC']],
     limit: 7
   }).then((books) => {
-    if(books) {
+    if (books) {
       res.render('index', {
-        books: books.rows, 
-        count: books.count, 
-        searched, 
+        books: books.rows,
+        count: books.count,
+        searched,
         route: 'searchpage',
-        title: `Search Results for "${query}"`, 
-        query});
-    }else {
+        title: `Search Results for "${query}"`,
+        query
+      });
+    } else {
       res.send(404, "Page Not Found!");
     }
   }).catch((err) => {
-      res.send(500);
+    res.send(500);
   });
 });
 
@@ -94,118 +101,123 @@ router.get('/searchpage', (req, res, next) => {
     where: {
       [Op.or]: [
         {
-        title: { 
-          [Op.like]: `%${query}%`}
+          title: {
+            [Op.like]: `%${query}%`
+          }
         },
         {
-        author:{ 
-          [Op.like]: `%${query}%`}
+          author: {
+            [Op.like]: `%${query}%`
+          }
         },
         {
-        genre: { 
-          [Op.like]:`%${query}%`}
+          genre: {
+            [Op.like]: `%${query}%`
+          }
         },
         {
-        year: { 
-          [Op.like]:`%${query}%`}
+          year: {
+            [Op.like]: `%${query}%`
+          }
         }
       ]
-    },  
+    },
     order: [['title', 'ASC']],
     offset: offset,
     limit: 7
   }).then((books) => {
-    if(books) {
+    if (books) {
       res.render('index', {
-        books: books.rows, 
-        count: books.count, 
-        searched, 
+        books: books.rows,
+        count: books.count,
+        searched,
         route: 'searchpage',
-        title: `Search Results for "${query}"`, 
-        query});
-    }else {
+        title: `Search Results for "${query}"`,
+        query
+      });
+    } else {
       res.send(404, "Page Not Found!");
     }
   }).catch((err) => {
-      res.send(500);
+    res.send(500);
   });
 });
 
 /*Post new book */
 router.post('/', (req, res, next) => {
   Book.create(req.body).then((book) => {
-    if(book) {
+    if (book) {
       res.redirect('/books/' + book.id);
-    }else {
+    } else {
       res.send(404);
     }
   }).catch((err) => {
-      if (err.name === "SequelizeValidationError") {
-        res.render('new-book', {book: Book.build(req.body), errors: err.errors});;
-      }else {
-        throw err;
-      }
+    if (err.name === "SequelizeValidationError") {
+      res.render('new-book', { book: Book.build(req.body), errors: err.errors });;
+    } else {
+      throw err;
+    }
   }).catch((err) => {
-      res.send(500);
+    res.send(500);
   });
 });
 
 /* Create new book form. */
 router.get('/new', (req, res, next) => {
-  res.render('new-book', {book: Book.build()});
+  res.render('new-book', { book: Book.build() });
 });
 
 
 /*Update book details*/
 router.post('/:id', (req, res, next) => {
-  Book.findById(req.params.id).then(function(book) {
-    if(book) {
-      return book.update(req.body); 
-    }else {
+  Book.findByPk(req.params.id).then((book) => {
+    if (book) {
+      return book.update(req.body);
+    } else {
       res.send(404);
     }
   }).then((book) => {
-      res.redirect(`/books/${book.id}`);
+    res.redirect(`/books/${book.id}`);
   }).catch((err) => {
     if (err.name === "SequelizeValidationError") {
       const book = req.body;
       book.id = req.params.id;
 
-      res.render('book-details', {book, errors: err.errors});;
-    }else {
+      res.render('book-details', { book, errors: err.errors });;
+    } else {
       throw err;
     }
-}).catch((err) => {
-      res.send(500);
+  }).catch((err) => {
+    res.send(500);
   })
 });
 
 /*Delete book*/
 router.post('/:id/delete', (req, res, next) => {
-  Book.findById(req.params.id).then((book) => {
-    if(book) {
+  Book.findByPk(req.params.id).then((book) => {
+    if (book) {
       return book.destroy();
-    }else {
+    } else {
       res.send(404);
     }
   }).then(() => {
-      res.redirect('/books');
+    res.redirect('/books');
   }).catch((err) => {
-      res.send(500);
+    res.send(500);
   })
 });
 
 /* GET individual book details. */
 router.get('/:id', (req, res, next) => {
-  Book.findById(req.params.id).then((book) => {
+  Book.findByPk(req.params.id).then((book) => {
     if (book) {
-      res.render('book-details', {book}); 
-    }else {
+      res.render('book-details', { book });
+    } else {
       res.send(404, "Sorry the book you are looking for was not found!");
     }
   }).catch((err) => {
-      res.send(500);
-  }); 
+    res.send(500);
+  });
 });
 
 module.exports = router;
